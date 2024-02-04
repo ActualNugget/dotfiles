c9046947877d2d08b726020ba9b636735e5b48da
ActualNugget <32017089+ActualNugget@users.noreply.github.com>
2024-02-04 14:08:12 +0000

    add: paulirish git shortcuts

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -10,6 +10,51 @@ alias cp 'command gcp --interactive --verbose'
 
 alias chmox='chmod +x'
 
+# `g co`, etc. subcommand expansion with `abbr`.
+function subcommand_abbr
+  set -l cmd "$argv[1]"
+  set -l short "$argv[2]"
+  set -l long "$argv[3]"
+
+  # Check that these strings are safe, since we're going to eval. 👺
+  if not string match --regex --quiet '^[a-z]*$' "$short"
+    or not string match --regex --quiet '^[a-z- ]*$' "$long"
+    echo "Scary unsupported alias or expansion $short $long"; exit 1; 
+  end
+
+  set -l abbr_temp_fn_name (string join "_" "abbr" "$cmd" "$short")
+  # Subcommand arg expanesion via commandline -tokenize + abbr --position anywhere
+  # thx lgarron for inspiration: https://github.com/lgarron/dotfiles/blob/2bc3e0282b/dotfiles/fish/.config/fish/abbr.fish & https://github.com/lgarron/dotfiles/blob/main/dotfiles/fish/.config/fish/dev.fish
+  # https://www.reddit.com/r/fishshell/comments/16s0bsi/leveraging_abbr_for_git_aliases/
+  set -l abbr_temp_fn "function $abbr_temp_fn_name
+    set --local tokens (commandline --tokenize)
+    if test \$tokens[1] = \"$cmd\"
+      echo $long
+    else
+      echo $short
+    end; 
+  end; 
+  abbr --add $short --position anywhere --function $abbr_temp_fn_name"
+  eval "$abbr_temp_fn"
+end
+
+subcommand_abbr git c "commit -am"
+subcommand_abbr git tc "commit -am"
+subcommand_abbr git cm "commit --no-all -m"
+subcommand_abbr git co "checkout"
+subcommand_abbr git c "commit -am"
+subcommand_abbr git s "status"
+subcommand_abbr git ts "status"
+subcommand_abbr git amend "commit --amend --all --no-edit"
+subcommand_abbr git hreset "reset --hard"
+subcommand_abbr git cp "cherry-pick"
+subcommand_abbr git cherrypick "cherry-pick"
+subcommand_abbr git dif "diff"
+
+# some of my git aliases
+subcommand_abbr git db "diffbranch"
+subcommand_abbr git dbt "diffbranch-that"
+
 # is it a `main` or a `master` repo?
 alias gitmainormaster="git branch --format '%(refname:short)' --sort=-committerdate --list master main | head -n1"
 alias main="git checkout (gitmainormaster)"
