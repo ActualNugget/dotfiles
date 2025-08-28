7a72d463288f8c33f7fe26179dc9eb3fe992f985
ActualNugget <32017089+ActualNugget@users.noreply.github.com>
2025-08-28 11:01:44 +0800

    fix: zoxide from dnf instead of custom install

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,5 @@
-sudo dnf install stow neovim kitty fish fortune-mod
+sudo dnf install stow neovim kitty fish fortune-mod zoxide
 curl -sS https://starship.rs/install.sh | sh
-curl -sSfL https://raw.githubusercontent.com/ajeetdsouza/zoxide/main/install.sh | sh
 git clone https://github.com/ActualNugget/dotfiles.git
 cd dotfiles
 stow .
