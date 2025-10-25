6dc3e0e116cf17e79e58e9bb46ad73322c01e460
ActualNugget <isvaran@tuta.io>
2025-10-25 16:08:11 +0800

    feat: find all files ignores content of /.git/

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -8,3 +8,12 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+-- Telescope
+-- change find all files to exclude .git
+map(
+  "n",
+  "<leader>fa",
+  "<cmd>Telescope find_files follow=true no_ignore=true find_command=rg,--files,--hidden,--glob,!**/.git/*<CR>",
+  { desc = "telescope find all files" }
+)
