958b975796edfc24641752ca02fef78a6d7fac90
ActualNugget <32017089+ActualNugget@users.noreply.github.com>
2026-02-01 12:00:30 +0800

    feat:basedpyright python lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,6 +1,15 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "ruff", "biome" }
+local servers = { "basedpyright", "ruff", "biome" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
+vim.lsp.config("basedpyright", {
+  settings = {
+    basedpyright = {
+      analysis = {
+        typeCheckingMode = "basic",
+      },
+    },
+  },
+})
